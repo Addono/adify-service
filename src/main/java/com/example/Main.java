a98419d4e534687fffc2dd22247988fc98333ea8
Adriaan Knapen <hi@aknapen.nl>
2020-12-01 11:01:10 +0100

    fix: adds missing HTTP endpoint
    Prevents Heroku from killing the dyno

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,18 @@
 package com.example;
 
-class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+  @RequestMapping("/")
+  String index() {
+    return "Hello, world!";
+  }
 
   public static void main(String[] args) throws Exception {
     Service.service(new Subscription[]{
@@ -9,6 +21,8 @@ class Main {
           adify.execute();
         })
     });
+    
+    SpringApplication.run(Main.class, args);
   }
 
 }
